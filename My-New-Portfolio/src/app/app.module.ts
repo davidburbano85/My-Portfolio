f8651b85a6134a627189aaea050a2d786f9a29da
David <davidrpy@gmail.com>
2022-07-20 21:39:37 -0500

    create rigth

--- My-New-Portfolio/src/app/app.module.ts
+++ My-New-Portfolio/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
+import { RigthComponent } from './components/rigth/rigth.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    RigthComponent
   ],
   imports: [
     BrowserModule,
